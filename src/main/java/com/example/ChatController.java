af91bc5a39814d9c11ae92d6cc2945d4a4c7934f
Michael <mikhail.stupko@gmail.com>
2018-12-02 23:07:03 +0400

    Working with 1 line

--- src/main/java/com/example/ChatController.java
+++ src/main/java/com/example/ChatController.java
@@ -4,21 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 
 
 @Controller
 public class ChatController {
-
+    private ArrayList <String> users = new ArrayList<>();
+    private ArrayList <String> messages = new ArrayList<>();
+    private String user;
+    Model model;
+    String fullName;
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(name = "fullName", required = false, defaultValue = "user") String name, Model model) {
         model.addAttribute("fullName", name);
+        this.model = model;
+        this.fullName = name;
         return "greeting";
     }
 
+
     @RequestMapping("/chat")
-    public String chatOpener(@RequestParam(name = "fullName", required = false, defaultValue = "user") String name, Model model) {
-        model.addAttribute("fullName", name);
+    public String chatOpener(@RequestParam(name = "fullName", required = false, defaultValue = "user") String name, @RequestParam(name = "message", required = false, defaultValue = "----") String message, Model model) {
+        if (fullName.equals("user")) fullName = name;
+        model.addAttribute("fullName", fullName);
+        model.addAttribute("message", message);
+        users.add(name);
+        messages.add(message);
+        this.model = model;
         return "chat";
     }
 
