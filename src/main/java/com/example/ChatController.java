632bfb8c203b4b2b282a5a7fadd43d153cb6f9da
Michael <mikhail.stupko@gmail.com>
2018-12-03 02:13:57 +0400

    Working chat in Thymeleaf

--- src/main/java/com/example/ChatController.java
+++ src/main/java/com/example/ChatController.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.*;
 import java.util.ArrayList;
 
 
@@ -12,8 +12,9 @@ public class ChatController {
     private static ArrayList <String> users = new ArrayList<>();
     private static ArrayList <String> messages = new ArrayList<>();
     private String user;
-    Model model;
-    String fullName;
+    //Model model;
+    private String fullName;
+    private StringBuilder stringBuilder = new StringBuilder();
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(name = "fullName", required = false, defaultValue = "user") String name, Model model) {
@@ -32,9 +33,16 @@ public class ChatController {
             users.remove("user");
             users.add(name);
         }
+
+        messages.add(Instant.now().atZone(ZoneId.of("Europe/Samara")).toString() + " | " + fullName + ": " + message);
+        stringBuilder.delete(0, stringBuilder.capacity());
+        messages.forEach((line) -> stringBuilder.append(line + "<br>"));
+
         model.addAttribute("fullName", fullName);
         model.addAttribute("message", message);
-        messages.add(message);
+        model.addAttribute("messages", stringBuilder.toString() + "<br>");
+
+
         //this.model = model;
         return "chat";
     }
